a1af7a84ae54019547e90347a4d935dba3b51178
maferojas23 <mafrojasva@unal.edu.co>
2025-05-17 08:47:54 -0500

    primer intento taller 4

---
+++ homework/__main__.py
@@ -0,0 +1,8 @@
+"""Entry point for the homework package."""
+
+# python3 -m homework  data/input data/output 
+
+from .src.main import WordCountApp
+
+if __name__ == "__main__":
+    WordCountApp().run()
